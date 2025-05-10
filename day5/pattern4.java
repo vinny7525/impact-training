3dfa415045b90fff74df29ac56176b9ede3a008d
vinny7525 <vinnysreeja.t@gmail.com>
2025-05-10 16:20:07 +0530

    Add files via upload

---
+++ day5/pattern4.java
@@ -0,0 +1,25 @@
+public class pattern4{
+    public static void main(String[] args) { 
+      for(int i=1;i<=3;i++)
+		{
+		    for(int j=1;j<=i;j++)
+		    {
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+      
+        for(int i=3;i>=0;i--)
+		{
+		    for(int j=0;j<=i;j++)
+		    {
+		        System.out.print("*");
+		    }
+		    System.out.println();
+
+
+		}
+	}
+        
+}   
+
